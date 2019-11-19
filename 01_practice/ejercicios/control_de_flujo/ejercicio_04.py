e88d2daa767d9bbae42bcd10b4e4db01700347f5
daniocen777 <danielochoa@outlook.com>
2019-11-19 15:46:21 -0500

    Ejercicios de flujos

--- 01_practice/ejercicios/control_de_flujo/ejercicio_04.py
+++ 01_practice/ejercicios/control_de_flujo/ejercicio_04.py
@@ -3,13 +3,14 @@ pares desde el 0 hasta el 100. '''
 
 numero = int(input("Ingrese el número \n"))
 suma = 0
-conteo = 1
+''' conteo = 1
 while (conteo <= numero):
     if (conteo % 2 == 0):
         suma += conteo
     conteo += 1
 
-print("La suma es:", suma)
-    
+print("La suma es:", suma) '''
 
-    
+''' Otra forma '''
+suma = sum(range(0, numero + 1, 2))
+print("La suma es:", suma)
