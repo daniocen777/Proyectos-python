a6efa5bad979099ba64136e2f5a5dd1a07e17ad2
daniocen777 <danielochoa@outlook.com>
2019-11-21 15:35:51 -0500

    Herencia 2

--- herencia/herencia.py
+++ herencia/herencia.py
@@ -20,7 +20,7 @@ class Adorno(Producto):
 
 adorno = Adorno(2014, 'Vaso adornado', 15,
                 'Vaso de porcelana adornado con árboles')
-print(adorno)
+''' print(adorno) '''
 
 
 class Alimento(Producto):
@@ -42,4 +42,23 @@ class Alimento(Producto):
 
 
 a = Alimento(2990, 'Pan', 34.22, 'Pan al pan', 'Panadería', 'Panadero')
-print(a)
+''' print(a) '''
+
+
+class Libro(Producto):
+    def __init__(self, referencia, nombre, precio, descripcion):
+        super().__init__(referencia, nombre, precio, descripcion)
+        self.sbn = sbn
+        self.autor = autor
+
+    def __str__(self):
+        return"""
+        REFERENCIA\t{}
+        NOMBRE\t\t{}
+        PRECIO\t\t{}
+        DESCRIPCIÓN\t{}
+        SBN\t{}
+        AUTOR\t{}
+        """.format(self.referecia, self.nombre, self.precio, self.descripcion, self.sbn, self.autor)
+
+l = Libro(2990, '2666', 89.22, 'Obra latinoamericana', '34444333', 'Pedro Páramo',)
